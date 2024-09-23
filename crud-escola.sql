8916445105ac935f5eb73f1f2b62f0f094f530ea
TTonia1 <161315439+TTonia1@users.noreply.github.com>
2024-09-23 08:33:04 -0300

    Banco de dados

--- crud-escola.sql
+++ crud-escola.sql
@@ -1,24 +1,24 @@
 create database escola;
 use escola;
 
-create table diario(
-id_diario INT not null  primary key auto_increment,
-hora_aula datetime,
-turma varchar(45)
-);
-
 create table professor(
 id_professor INT not null  primary key auto_increment,
 nome_professor varchar(45),
 data_nascimento date,
 CPF char(11),
-materia varchar(45),
-id_diario INT NOT NULL,
-FOREIGN KEY (id_diario) REFERENCES diario(id_diario)
+materia varchar(45)
 );
 
 create table aulas(
-id_aula INT not null  primary key auto_increment,
-id_diario INT NOT NULL,
-FOREIGN KEY (id_diario) REFERENCES diario(id_diario)
+id_aula INT not null  primary key auto_increment
+);
+
+create table diario(
+id_diario INT not null  primary key auto_increment,
+hora_aula datetime,
+turma varchar(45),
+id_professor INT NOT NULL,
+FOREIGN KEY (id_professor) REFERENCES professor(id_professor),
+id_aula INT NOT NULL,
+FOREIGN KEY (id_aula) REFERENCES aulas(id_aula)
 );
